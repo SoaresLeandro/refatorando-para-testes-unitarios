1d6bc8248e73869189577ad62c141eb9a634fdac
Leandro <lss.91@live.com>
2024-12-11 16:06:27 -0400

    Criando os Commands

---
+++ Store.Domain/Commands/CreateOrderCommnad.cs
@@ -0,0 +1,36 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+using Store.Domain.Commands.Interfaces;
+
+namespace Store.Domain.Commands;
+
+public class CreateOrderCommnad : Notifiable<Notification>, Icommand
+{
+    public CreateOrderCommnad()
+    {
+        Items = new List<CreateOrderItemCommand>();
+    }
+    
+    public CreateOrderCommnad(string customer, string zipCode, string promoCode, IList<CreateOrderItemCommand> items)
+    {
+        Customer = customer;
+        ZipCode = zipCode;
+        PromoCode = promoCode;
+        Items = items;
+    }
+
+    public string Customer { get; set; }
+    public string ZipCode { get; set; }
+    public string PromoCode { get; set; }
+    public IList<CreateOrderItemCommand> Items { get; set; }
+
+    public void Validate()
+    {
+        AddNotifications(
+            new Contract<CreateOrderCommnad>()
+            .Requires()
+            .AreEquals(Customer.Length, 11, "Customer", "Cliente inválido")
+            .AreEquals(ZipCode, 8, "ZipCode", "CEP inválido")
+        );
+    }
+}
--- Store.Tests/obj/Debug/net9.0/Store.Tests.AssemblyInfo.cs
+++ Store.Tests/obj/Debug/net9.0/Store.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8a5b06c70020f04d1e50a33b1c806162614103")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+462dc399046d57d40d1a59a6154b8ae47708083b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Store.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
